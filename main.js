916438c6ef7e98c9158ab83fb8c579c0cbc1016f
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-02-01 20:23:34 +0000

    Fix spelling mistake in multiply function

--- main.js
+++ main.js
@@ -8,11 +8,9 @@ function add(a, b) {
 function subtract(a, b) {
   return a - b;
 }
-function multiplb(a, b) {
+function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
   return a / b;
 }
-
-console.log(numberA);
