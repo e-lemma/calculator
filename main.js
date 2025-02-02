5136c79623d6d3de4e118e60e4601bd8f32669bc
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-02-02 18:44:35 +0000

    Replace invalid HTML entity reference with actual character in operator mapping

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ function operate(a, operator, b) {
   const operatorMapping = {
     "+": add(a, b),
     "-": subtract(a, b),
-    "&times": multiply(a, b),
-    "&divide": divide(a, b),
+    "×": multiply(a, b),
+    "÷": divide(a, b),
   };
 
   return operatorMapping[operator];
