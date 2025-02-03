e281a91c100201cf74d503484f323e080b5fd98e
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-02-03 16:38:23 +0000

    Add error handling in divide() to prevent crashes

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
+  if (b === 0) {
+    alert("Cannot divide by Zero!!");
+    resetVariables();
+    return "Error";
+  }
   return a / b;
 }
 
